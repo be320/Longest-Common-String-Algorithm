9314bc7ed0447e706e1ef13db816c8fccd244cbc
be320 <ahmedbahaa.3@hotmail.com>
2020-03-06 16:55:53 +0200

    Matrix Visualized

--- lcstring/src/App.jsx
+++ lcstring/src/App.jsx
@@ -25,7 +25,26 @@ function App() {
     console.log(matrix)
     if(draw){
     return(
-      <div>hello world</div>
+      <ArcherContainer strokeColor='rgba(255, 0, 0,0.7)'  arrowThickness={10} strokeWidth={1} >
+      <div className="table">
+        <table cellPadding="10px" cellSpacing="10px" >
+          <tbody>
+          {
+    matrix.map((value,index) => 
+    <tr key={index}>
+          {  value.map(
+              (chr,ind) => <td key={ind}>{chr}</td>
+            )
+          }
+            </tr>
+    )
+}
+          </tbody>
+        </table>
+      </div>
+
+</ArcherContainer>
+
     )
     }
     else
@@ -39,27 +58,45 @@ function App() {
   const run = () => {
     const hor = seq1.split('')
     const ver = seq2.split('')
-    const lcsMatrix = Array(ver.length+1).fill(null).map(
+    const lcsMatrix = Array(ver.length+2).fill(null).map(
       ()=>
-      Array(hor.length+1).fill(null)
+      Array(hor.length+2).fill(null)
     )
     
-    //filling first row with zeroes
-    for(let colIndex=0;colIndex<=hor.length;colIndex++)
+    //filling seq1 in first row
+    for(let colIndex=0;colIndex<=hor.length+1;colIndex++)
     {
-      lcsMatrix[0][colIndex] =0;
+      if(colIndex<2)
+      lcsMatrix[0][colIndex] ='';
+      else
+      lcsMatrix[0][colIndex] =hor[colIndex-2];
     }
 
-    //fillimg first column with zeroes
-    for(let rowIndex=0;rowIndex<= ver.length;rowIndex++)
+    //filling seq2 in first col
+    for(let rowIndex=0;rowIndex<=ver.length+1;rowIndex++)
     {
-      lcsMatrix[rowIndex][0]=0;
+      if(rowIndex<2)
+      lcsMatrix[rowIndex][0] ='';
+      else
+      lcsMatrix[rowIndex][0] =ver[rowIndex-2];
+    }
+
+    //filling second row with zeroes
+    for(let colIndex=1;colIndex<=hor.length+1;colIndex++)
+    {
+      lcsMatrix[1][colIndex] =0;
+    }
+
+    //fillimg second column with zeroes
+    for(let rowIndex=1;rowIndex<= ver.length+1;rowIndex++)
+    {
+      lcsMatrix[rowIndex][1]=0;
     }
 
       // Fill rest of the column that correspond to each of two strings.
-  for (let rowIndex = 1; rowIndex <= ver.length; rowIndex ++) {
-    for (let colIndex = 1; colIndex <= hor.length; colIndex ++) {
-      if (hor[colIndex - 1] === ver[rowIndex - 1]) { //matching top-left arrow
+  for (let rowIndex = 2; rowIndex <= ver.length+1; rowIndex ++) {
+    for (let colIndex = 2; colIndex <= hor.length+1; colIndex ++) {
+      if (hor[colIndex - 2] === ver[rowIndex - 2]) { //matching top-left arrow
         lcsMatrix[rowIndex][colIndex] = lcsMatrix[rowIndex - 1][colIndex - 1] + 1;
         //draw top left arrow here
       } else if(lcsMatrix[rowIndex - 1][colIndex] >=  lcsMatrix[rowIndex][colIndex - 1]) {
